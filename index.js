fd9928961bc5ef729777b90b6961cb23364c9258
rotembab <rotembabmen@gmail.com>
2023-08-27 19:23:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const routes = require("./routes/routes");
+const express = require("express");
+const mongoose = require("mongoose");
+const mongoString = process.env.DB_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Connected to DB");
+});
+
+const app = express();
+app.use(express.json);
+app.listen(8080, () => {
+  console.log("Server is listening on PORT:8080 ");
+});
+
+app.use("/api", routes);
