df3b217a238fdcccf9c1b7885586dac7131a6ec8
rotembab <rotembabmen@gmail.com>
2023-09-03 17:44:04 +0300

    movies schema

---
+++ models/movies.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const dataSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  describtion: {
+    required: true,
+    type: String,
+  },
+  imgSrc: {
+    required: true,
+    type: String,
+  },
+  releaseDate: {
+    required: true,
+    type: Date,
+  },
+  movieLen: {
+    required: true,
+    type: Number, //In minutes
+  },
+  lang: {
+    required: true,
+    type: String,
+  },
+  options: [
+    {
+      required: true,
+      enum: ["2D", "3D", "4DX", "SCREENX", "IMAX"],
+    },
+  ],
+  ageStrict: {
+    required: false,
+    type: Number,
+  },
+});
